a03bf2fe60d6929d9e16d60568d73757cb081cc2
Negmatov MirzoUlugbek <votamgen@Gmail.comu.negmatov@Gmail.com>
2018-04-20 12:49:09 +0500

    __________________________________v..3 + add: dict for create .JSON in future + add: downloader for images + add: identify_number
    ? need: reCapcha parser

--- Web request2.py
+++ Web request2.py
@@ -3,37 +3,32 @@ import re
 from bs4 import BeautifulSoup  # for parsing HTML
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-""" region httpRequest
+out_dict = dict()
+out_dict['properties'] = []
+
+# region httpRequest
 http = urllib3.PoolManager()
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                          "(KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36",
            'Referer': 'https://www.mediamarkt.ru/item/1361208/apple-iphone-x-256gb-space-gray-smartfon',
-           # 'cache-control': 'max-age=3600',
-           # 'content-encoding': 'gzip',
-           # 'content-type': 'text/css',
-           # 'date': 'Thu, 12 Apr 2018 07:05:57 GMT',
-           # 'etag': 'W/"5acda7ef-3570"',
-           # 'expires': 'Thu, 12 Apr 2018 08:05:57 GMT',
-           # 'last-modified': 'Wed, 11 Apr 2018 06:15:11 GMT',
-           # 'server': 'nginx/1.12.2',
-           # 'status': '304',
-           # 'x-assets': '1'
            }
 response = http.request('GET',
                         'https://www.mediamarkt.ru/item/1361208/apple-iphone-x-256gb-space-gray-smartfon#card-features',
                         headers=headers)
 
 response = str(response.data, encoding='utf8')
-endregion"""
+# endregion
 
-response = open('jout_data.html', encoding='utf8', mode='r').readlines()
-response = ''.join(response)
+# response = open('jout_data2.html', encoding='utf8', mode='r').readlines()
+# response = ''.join(response)
+with open('out_data2.html', encoding='utf8', mode='w') as f:
+    f.write(response)
+    f.close()
 
 soup = BeautifulSoup(response, 'html.parser')
 
-"""
+# Getting CHARACTERISTICS of product
 characteristics = soup.find('div', {'class': 'characteristics__group additional'})
-
 if characteristics:
     for li in characteristics.find_all('li'):
         # print(li.find_all("span"))
@@ -41,25 +36,57 @@ if characteristics:
             a = str(li.find_all('span')[0])
             b = str(li.find_all('span')[1].string).strip()
 
-            rex = re.compile(r"<mm-tooltip.*?>(?P<tooltip>.*?)</mm-tooltip>", re.DOTALL)
-            result = rex.search(a)
-            if result: print(f'tooltip: {result.group("tooltip")}')
-
             rex = re.compile(r"<span.+?>(?P<text>.+?)<", re.DOTALL)
             result = rex.search(a)
-            if result: print(f"text: {result.group('text').strip()} :b: {b}")
+            if result:
+                print(f"text: {result.group('text').strip()} :b: {b}")
+                out_dict['characteristics'].append([result.group('text').strip(), b])
+
+            rex = re.compile(r"<mm-tooltip.*?>(?P<tooltip>.*?)</mm-tooltip>", re.DOTALL)
+            result = rex.search(a)
+            if result:
+                print(f'tooltip: {result.group("tooltip")}')
+                out_dict['characteristics'][-1].append(result.group('tooltip'))
 
         else:
             print('GROUP:', li.string)
+            out_dict['characteristics'].append(li.string)
 
-
+# Getting DESCRIPTION
 description = soup.find('div', {"class": "reveal-block__content reveal-block__content--block"})
 description = str(description.prettify())
 description = re.sub(r"<div.*?>", '', description)
 description = re.sub(r"</div>", '', description)
-"""
+out_dict['description'] = description
+print(out_dict['description'])
 
+# Getting IMAGE LINKS
+out_dict['img_link'] = []
 for photo in soup.find_all('a',  {'class': "bullet"}):
-    print(photo['data-ng-click'], end='\n\n\n')
+    if photo.get('data-ng-click'):
+        rex = re.compile(r".*showBigImage.*?event,.*?\'(?P<img_link>http.*?)\'.*?bullet=(?P<num>[0-9]+)", re.DOTALL)
+        result = rex.search(photo['data-ng-click'])
+        if result:
+            print(f"img_link: {result.group('img_link').strip()}; Bullet={result.group('num')}")
+            out_dict['img_link'].append([result.group('img_link').strip(),
+                                         result.group('num')])
 # photos
+
+# Getting IDENTIFICATION number
+id_number = soup.find('div', {'class': 'card__article'}).div['id']
+print(f"АРТИКУЛ: {id_number}")
+out_dict['id_number'] = id_number
+
+# Getting IMAGE from LINKS
+for link in out_dict['img_link']:
+    response = http.request('GET', link[0], headers=headers, preload_content=False)
+    path = r"c:\Users\LocalAdmin\_JuPyteR\data\\" \
+           f"{id_number}-{out_dict['img_link'][1]}"
+    with open(path, 'wb') as out:
+        while True:
+            data = response.read(1024)
+            if not data:
+                break
+            out.write(data)
+    response.release_conn()
 
--- test.py
+++ test.py
@@ -1,2 +1,16 @@
-import re
-print(re.escape())
+# import re
+# print(re.escape())
+
+
+out_data = [
+    ['Group_name1'],
+    ['data1', 'data2', 'tooltip'],
+    ['data1', 'data2', 'tooltip'],
+    ['data1', 'data2', 'tooltip'],
+    ['Group_name'],
+    ['data1', 'data2', 'tooltip'],
+    ['data1', 'data2', 'tooltip'],
+    ['data1', 'data2', 'tooltip'],
+    ...
+]
+
